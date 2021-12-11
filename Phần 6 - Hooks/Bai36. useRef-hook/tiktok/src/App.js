e07ac222bc795035a8a77721f69953c5cb9d8fbb
thanhyou00 <truc06082000@gmail.com>
2021-12-11 12:16:15 +0700

    thanhyou00

---
+++ Phần 6 - Hooks/Bai36. useRef-hook/tiktok/src/App.js
@@ -0,0 +1,50 @@
+
+import { useRef, useState, useEffect } from 'react'
+
+// Lưu các giá trị qua một tham chiếu bên ngoài function component
+
+
+function App() {
+  const [count, setCount] = useState(60);
+
+  const timerId = useRef();
+  const prevCount = useRef();
+  const h1Ref = useRef();
+
+
+  useEffect(()=>{
+    prevCount.current = count;
+  },[count])
+
+  useEffect(() => {
+   // const rect = h1Ref.current.getBoundingClientRect();
+
+    console.log(h1Ref.current);
+  }) 
+
+  const handleStart = ()=>{
+      timerId.current = setInterval(()=>{
+        setCount(preCount => preCount - 1);
+      },1000)
+
+      console.log('Start -> ', timerId.current);
+  }
+
+  const handleStop = ()=>{
+      clearInterval(timerId.current);
+      console.log('Stop -> ', timerId.current);     
+  } 
+  
+  console.log(count, prevCount.current);
+
+  return (
+    <div style={{ padding : 20}}>
+        <h1 ref={h1Ref}>{count}</h1>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+    </div>
+  )
+
+}
+
+export default App;
