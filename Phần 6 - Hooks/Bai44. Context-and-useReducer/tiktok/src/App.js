9c6d4e1fabcac5996bdf9aafcd5eeb8cc00386a8
thanhyou00 <truc06082000@gmail.com>
2021-12-13 23:44:32 +0700

    thanhyou00

---
+++ Phần 6 - Hooks/Bai44. Context-and-useReducer/tiktok/src/App.js
@@ -0,0 +1,30 @@
+import { useStore, actions } from './store'
+
+function App() {
+  const [state, dispatch] = useStore();
+  const { todos, todoInput } = state
+  console.log('todoInput', todoInput);
+
+  const handleAdd = () => {
+      dispatch(actions.addTodo(todoInput))
+  }
+
+  return (
+    <div style={{padding: '10px 32px'}}>
+        <input 
+          value={todoInput}
+          placeholder='Enter todo...'
+          onChange={e=>{
+            dispatch(actions.setTodoInput(e.target.value))
+          }}
+        />
+        <button onClick={handleAdd}>Add</button>
+        {todos.map((todo, index)=>(
+          <li key={index}>{todo}</li>
+        ))}
+    </div>
+  )
+
+}
+
+export default App;
